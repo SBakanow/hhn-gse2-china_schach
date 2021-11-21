7e42fac52ba70a5760ea8e8cb4d6f16fe578111b
Marvin Simon <simon.marvin.1@web.de>
2021-11-21 20:56:28 +0100

    Revesed that atrocious looking formatting :)

--- src/china_schach/Elefant.java
+++ src/china_schach/Elefant.java
@@ -1,65 +1,64 @@
-
 import greenfoot.*;
 
 
 public final class Elefant extends Spielfigur {
-    public Elefant(final boolean farbeIstRot) {
-        super(farbeIstRot);
+  public Elefant(final boolean farbeIstRot) {
+    super(farbeIstRot);
 
-        if (farbeIstRot) {
-            setImage("English-Elephant-Red.png");
-        } else {
-            setImage("English-Elephant-Black.png");
-        }
+    if (farbeIstRot) {
+      setImage("English-Elephant-Red.png");
+    } else {
+      setImage("English-Elephant-Black.png");
+    }
+  }
+
+  public void act() {
+    if (Greenfoot.mouseDragged(this)) {
+      setLocation(Greenfoot.getMouseInfo().getX(),
+          Greenfoot.getMouseInfo().getY());
+    }
+    if (Greenfoot.mouseDragEnded(this)) {
+      bewegen((Schnittpunkt) getOneIntersectingObject(Schnittpunkt.class));
+    }
+  }
+
+  public void bewegen(final Schnittpunkt ziel) {
+    if (ziel != null && istBewegungErlaubt(ziel)) {
+      setLocation(((Actor) ziel).getX(), ((Actor) ziel).getY());
+      oldX = getX();
+      oldY = getY();
+    } else {
+      setLocation(oldX, oldY);
+      oldX = getX();
+      oldY = getY();
+    }
+  }
+
+
+  private boolean istBewegungErlaubt(final Schnittpunkt ziel) {
+    if (position.getPunkttyp() == Punkttyp.FLUSS &&
+        (position.getZeile() - ziel.getZeile() == -2) &&
+        Math.abs((int) position.getSpalte() - ziel.getSpalte()) == 2 &&
+        farbeIstRot) {
+      setPosition();
+      position = ziel;
+      return true;
+    } else if (position.getPunkttyp() == Punkttyp.FLUSS &&
+        (position.getZeile() - ziel.getZeile() == 2) &&
+        Math.abs((int) position.getSpalte() - ziel.getSpalte()) == 2 &&
+        !farbeIstRot) {
+      setPosition();
+      position = ziel;
+      return true;
+    } else if (Math.abs(position.getZeile() - ziel.getZeile()) == 2 &&
+        Math.abs(((int) position.getSpalte() - ziel.getSpalte())) == 2 &&
+        position.getPunkttyp() != Punkttyp.FLUSS) {
+      setPosition();
+      position = ziel;
+      return true;
     }
 
-    public void act() {
-        if (Greenfoot.mouseDragged(this)) {
-            setLocation(Greenfoot.getMouseInfo().getX(),
-                        Greenfoot.getMouseInfo().getY());
-        }
-        if (Greenfoot.mouseDragEnded(this)) {
-            bewegen((Schnittpunkt)getOneIntersectingObject(Schnittpunkt.class));
-        }
-    }
-
-    public void bewegen(final Schnittpunkt ziel) {
-        if (ziel != null && istBewegungErlaubt(ziel)) {
-            setLocation(((Actor)ziel).getX(), ((Actor)ziel).getY());
-            oldX = getX();
-            oldY = getY();
-        } else {
-            setLocation(oldX, oldY);
-            oldX = getX();
-            oldY = getY();
-        }
-    }
-
-
-    private boolean istBewegungErlaubt(final Schnittpunkt ziel) {
-        if (position.getPunkttyp() == Punkttyp.FLUSS                    &&
-            (position.getZeile() - ziel.getZeile() == -2)               &&
-            Math.abs((int)position.getSpalte() - ziel.getSpalte()) == 2 &&
-            farbeIstRot) {
-            setPosition();
-            position = ziel;
-            return true;
-        } else if (position.getPunkttyp() == Punkttyp.FLUSS                     &&
-                   (position.getZeile() - ziel.getZeile() == 2)                 &&
-                   Math.abs((int)position.getSpalte() - ziel.getSpalte()) == 2  &&
-                   !farbeIstRot) {
-            setPosition();
-            position = ziel;
-            return true;
-        } else if (Math.abs(position.getZeile() - ziel.getZeile()) == 2             &&
-                   Math.abs(((int)position.getSpalte() - ziel.getSpalte())) == 2    &&
-                   position.getPunkttyp() != Punkttyp.FLUSS) {
-            setPosition();
-            position = ziel;
-            return true;
-        }
-
-        return false;
-    }
+    return false;
+  }
 }
 
